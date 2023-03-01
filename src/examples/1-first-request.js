283536e14b38613d7747d3085ba4d7c371b4479e
shubh-37 <shubh.arya37@gmail.com>
2023-03-01 13:03:54 +0530

    implemented get request to access the data

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,10 +1,22 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 // limit, if 429 wait for 15 min and try again
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+  const authReq = async () => {
+    try {
+      const response = await axios(url);
+      console.log(response);
+      const data = response.data;
+      console.log(data[0].name);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   useEffect(() => {
-    console.log('first axios request');
+    authReq();
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
